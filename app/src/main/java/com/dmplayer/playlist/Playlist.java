5be46e97f774c736c22c2696e05caa498e9cb1a1
eldarium <eldarium@gmail.com>
2016-08-03 16:29:14 +0300

    black outline

---
+++ app/src/main/java/com/dmplayer/playlist/Playlist.java
@@ -0,0 +1,13 @@
+package com.dmplayer.playlist;
+
+import com.dmplayer.models.SongDetail;
+
+/**
+ * Created by asus on 03.08.2016.
+ */
+public class Playlist {
+
+    private String name;
+    private SongDetail[] songs;
+
+}
