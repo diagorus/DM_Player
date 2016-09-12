02f0d0972c4c7eac728f7582ad9bab561873c49b
Alexvojander <alexvojander@gmail.com>
2016-09-12 17:56:53 +0300

    streaming v2

--- app/src/main/java/com/dmplayer/fragments/FragmentStream.java
+++ app/src/main/java/com/dmplayer/fragments/FragmentStream.java
@@ -5,74 +5,51 @@
  */
 package com.dmplayer.fragments;
 
-import com.dmplayer.R;
-import com.dmplayer.manager.MediaController;
-import com.dmplayer.models.SongDetail;
-
-import android.annotation.TargetApi;
-import android.app.ProgressDialog;
-import android.media.MediaPlayer;
-import android.os.Build;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
-import android.app.Fragment;
-import android.text.Layout;
-import android.util.Log;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Switch;
-import android.widget.TextView;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.InterfaceAddress;
-import java.net.MalformedURLException;
-import java.net.NetworkInterface;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
-import java.util.Arrays;
-import java.util.Enumeration;
+import com.dmplayer.R;
+import com.dmplayer.manager.MediaController;
+import com.dmplayer.streamaudio.ClientUDPThread;
+import com.dmplayer.streamaudio.ServerUDPThread;
 
 public class FragmentStream extends Fragment {
 
-	Button sendButton;
-	EditText editTextIpAdress,editTextPortAdress;
-	SongDetail audioInfo = MediaController.getInstance().getPlayingSongDetail();
-	String ipAdress,path1;
-	String LOG_TAG="LOG_TAG:";
-	Socket socket;
-	Switch switcher;
-	String port;
-	MediaPlayer mediaPlayer = new MediaPlayer();
-	ServerSocket serverSocket;
-	static final int SocketServerPORT = 5005;
-	TextView ipTextView,portTextView;
-	LinearLayout clientLayout,serverLayout;
+	//public static TextView ipTextView;
+	Button stopStreamButton;
+	//EditText editTextIpAdress,editTextPortAdress;
+	//SongDetail audioInfo = MediaController.getInstance().getPlayingSongDetail();
+	//String ipAdress,path1;
 
-	int totalRead;
+	Switch switcher;
+	//String port="5005";
+	//TextView ipTextView,portTextView;
+	//LinearLayout clientLayout,serverLayout;
+	Button pauseButton,stopButton;
+	Button connectToServerButton;
+	ServerUDPThread serverUDPThread;
+	ClientUDPThread clientUDPThread;
+
+	//Handler handler = new Handler();
+
+	ProgressBar pb,pbClient;
+	Handler handler;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootview = inflater.inflate(R.layout.fragment_stream, null);
 		setupInitialViews(rootview);
 		return rootview;
 	}
-
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
@@ -80,291 +57,104 @@ public class FragmentStream extends Fragment {
 
 	private void setupInitialViews(View view) {
 
-		editTextIpAdress = (EditText)view.findViewById(R.id.ip_stream_server);
-		editTextPortAdress=(EditText)view.findViewById(R.id.port_stream_server);
+		//editTextIpAdress = (EditText)view.findViewById(R.id.ip_stream_server);
+		//editTextPortAdress=(EditText)view.findViewById(R.id.port_stream_server);
+		stopStreamButton=(Button)view.findViewById(R.id.stop_stream_music);
+		stopStreamButton.setOnClickListener (startListener);
+		pbClient=(ProgressBar)view.findViewById(R.id.progressBarClient);
 
-		sendButton=(Button)view.findViewById(R.id.send_stream_server);
-		sendButton.setOnClickListener (startListener);
+		pb=(ProgressBar)view.findViewById(R.id.progressBar);
+		pauseButton=(Button)view.findViewById(R.id.pause_button);
+		stopButton=(Button)view.findViewById(R.id.stop_button);
+		connectToServerButton=(Button)view.findViewById(R.id.connect_to_server_button);
+		connectToServerButton.setOnClickListener(connectToServerListener);
 		switcher=(Switch)view.findViewById(R.id.switchServer);
 		switcher.setOnCheckedChangeListener(serverListener);
-
-		clientLayout=(LinearLayout)view.findViewById(R.id.clientLayout);
-		serverLayout=(LinearLayout)view.findViewById(R.id.serverLayout);
-
-
-		ipTextView=(TextView)view.findViewById(R.id.serverIp);
-		portTextView=(TextView)view.findViewById(R.id.serverPort);
+		//clientLayout=(LinearLayout)view.findViewById(R.id.clientLayout);
+		//serverLayout=(LinearLayout)view.findViewById(R.id.serverLayout);
+		//ipTextView=(TextView)view.findViewById(R.id.serverIp);
+		//portTextView=(TextView)view.findViewById(R.id.serverPort);
 	}
-	private final CompoundButton.OnCheckedChangeListener serverListener = new CompoundButton.OnCheckedChangeListener() {
+	private CompoundButton.OnCheckedChangeListener serverListener = new CompoundButton.OnCheckedChangeListener() {
+
 		@Override
 		public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 			if(switcher.isChecked()){
-				serverLayout.setEnabled(true);
-				clientLayout.setEnabled(false);
-				sendButton.setEnabled(false);
+
+				//sendButton.setEnabled(false);
 				switcher.setText("Server ON");
-				RecvAudio();
+				stopButton.setEnabled(true);
+				pauseButton.setEnabled(true);
+				startReciveAudioSocket(pb,getContext());
+
+
+
 			}
 			else{
-				serverLayout.setEnabled(false);
-				clientLayout.setEnabled(true);
-				sendButton.setEnabled(true);
+				stopButton.setEnabled(false);
+				pauseButton.setEnabled(false);
+				//sendButton.setEnabled(true);
 				switcher.setText("Server OFF");
+				if (serverUDPThread != null) {
+					if(serverUDPThread.sockett!=null){serverUDPThread.sockett.close();}
+					Thread dummy = serverUDPThread;
+					serverUDPThread = null;
+					dummy.interrupt();
+				}
 			}
 		}
 	};
-	private final View.OnClickListener startListener = new View.OnClickListener() {
+	private  View.OnClickListener connectToServerListener = new View.OnClickListener() {
 
 		@Override
 		public void onClick(View arg0) {
-			//status = true;
-			//startStreaming();
-			ipAdress=editTextIpAdress.getText().toString();
-			if(audioInfo!=null){
-				path1=audioInfo.getPath();
-				port=editTextPortAdress.getText().toString();
-				SendAudio();
-			}else{
-				//song not playing
-			}
+			startSendAudioSocket(pbClient);
 		}
 	};
-	public void SendAudio()
-	{
-		Thread thrd = new Thread(new Runnable() {
-			@Override
-			public void run()
-			{
-				Log.e(LOG_TAG, "start send thread, thread id: "
-						+ Thread.currentThread().getId());
+	private View.OnClickListener startListener = new View.OnClickListener() {
 
-				try {
-					//File bufferFile = File.createTempFile("test", "mp3");
-					//BufferedOutputStream bufferOS = new BufferedOutputStream(new FileOutputStream(bufferFile));
+		@Override
+		public void onClick(View arg0) {
 
-					InputStream is =  new FileInputStream(path1);
-					BufferedInputStream bis = new BufferedInputStream(is);
-
-					byte[] buffer = new byte[32384];
-					byte[] buffer2 = new byte[32384];
-					int numRead;
-					boolean started = false;
-
-					InetAddress address = InetAddress.getByName(ipAdress);
-					//DatagramSocket datagramSocket = new DatagramSocket();
-//100.76.119.103
-					totalRead=0;
-
-
-					while ((numRead = bis.read(buffer)) != -1) {
-						socket = new Socket(address,Integer.parseInt(port));
-						OutputStream out = socket.getOutputStream();
-						DataOutputStream dos = new DataOutputStream(out);
-
-						InputStream in=socket.getInputStream();
-						DataInputStream dis = new DataInputStream(in);
-//100.86.102.81
-						dos.writeInt(numRead);
-						dos.write(buffer, 0, numRead);
-
-						int len = dis.readInt();
-						Log.d(LOG_TAG, "len " +len);
-						if (len > 0) {
-							dis.readFully(buffer2,0,len);
-						}
-						if(Arrays.equals(buffer,buffer2)){
-							dos.writeBoolean(true);
-							totalRead += numRead;
-
-							Log.d(LOG_TAG, "bytes_send(numRead) : " + numRead);
-							Log.d(LOG_TAG, "totalRead : " + totalRead);
-							Log.d(LOG_TAG, "file size : " + new File(path1).length());
-
-							if (totalRead >=new File(path1).length() && !started) {
-								Log.e("Player", "BufferHIT:StartPlay");
-								totalRead=0;
-							}
-						}
-						else{
-							dos.writeBoolean(false);
-
-						}
-						out.close();
-					}
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			} // end run
-		});
-		thrd.start();
-	}
-
-	Handler handler = new Handler();
-	public void RecvAudio()
-	{
-
-		Thread thrd = new Thread(new Runnable() {
-			@Override
-			public void run()
-			{
-				Boolean bool=false;
-				try {
-
-					serverSocket = new ServerSocket(SocketServerPORT);
-
-					handler.post(new Runnable(){
-						public void run() {
-							try {
-								ipTextView.setText("Ip: "+ip());
-							} catch (SocketException e) {
-								e.printStackTrace();
-							}
-							portTextView.setText("Port: "+serverSocket.getLocalPort());
-
-						}
-					});
-
-
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-				//FileOutputStream audio_stream=null;
-
-				//byte[] totalbyte=null;
-
-				byte[] buffer = new byte[32384];
-				try {
-					File bufferFile = File.createTempFile("test", "mp3");
-					BufferedOutputStream bufferOS = new BufferedOutputStream(new FileOutputStream(bufferFile));
-
-					boolean started = false;
-					while(true)
-					{
-						Socket socket = serverSocket.accept();
-						if(mediaPlayer.isPlaying()){
-							mediaPlayer.stop();
-							mediaPlayer=new MediaPlayer();
-
-						}
-						InputStream in = socket.getInputStream();
-						DataInputStream dis = new DataInputStream(in);
-
-						OutputStream out = socket.getOutputStream();
-						DataOutputStream dos = new DataOutputStream(out);
-						int len = dis.readInt();
-						Log.d(LOG_TAG, "len " +len);
-						if (len > 0) {
-							dis.readFully(buffer,0,len);
-						}
-						dos.writeInt(len);
-						dos.write(buffer, 0, len);
-						bool=dis.readBoolean();
-						if(bool){
-							Log.d(LOG_TAG, "recive2 pack: " + buffer.length);
-							bufferOS.write(buffer, 0,len);
-							bufferOS.flush();
-							totalRead+=len;
-
-							Log.d(LOG_TAG, "recive2 pack: " + buffer.length);
-							Log.d(LOG_TAG, "bytes_count : " + totalRead);
-							//Log.d(LOG_TAG, "file size : " + new File("storage/emulated/0/Download/Mmdance.mp3").length());
-
-							if (len<32384 ) {
-								Log.e("Player", "BufferHIT:StartPlay");
-								if(mediaPlayer.isPlaying())
-								{
-									mediaPlayer.stop();
-									mediaPlayer=new MediaPlayer();
-								}
-								setSourceAndStartPlay(bufferFile, totalRead);
-								totalRead=0;
-
-								bufferFile = File.createTempFile("test", "mp3");
-								bufferOS = new BufferedOutputStream(new FileOutputStream(bufferFile));
-
-							}
-						}
-					}
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			} // end run
-		});
-		thrd.start();
-
-	}
-	private String getIpAddress() {
-		String ip = "";
-		try {
-			Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
-					.getNetworkInterfaces();
-			while (enumNetworkInterfaces.hasMoreElements()) {
-				NetworkInterface networkInterface = enumNetworkInterfaces
-						.nextElement();
-				Enumeration<InetAddress> enumInetAddress = networkInterface
-						.getInetAddresses();
-				while (enumInetAddress.hasMoreElements()) {
-					InetAddress inetAddress = enumInetAddress.nextElement();
-
-					if (inetAddress.isSiteLocalAddress()) {
-						ip += "SiteLocalAddress: "
-								+ inetAddress.getHostAddress() + "\n";
-					}
-
-				}
-
-			}
-
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			ip += "Something Wrong! " + e.toString() + "\n";
-		}
-
-		return ip;
-	}
-	public InetAddress ip() throws SocketException {
-		Enumeration<NetworkInterface> nis = NetworkInterface.getNetworkInterfaces();
-		NetworkInterface ni;
-		while (nis.hasMoreElements()) {
-			ni = nis.nextElement();
-			if (!ni.isLoopback()/*not loopback*/ && ni.isUp()/*it works now*/) {
-				for (InterfaceAddress ia : ni.getInterfaceAddresses()) {
-					//filter for ipv4/ipv6
-					if (ia.getAddress().getAddress().length == 4) {
-						//4 for ipv4, 16 for ipv6
-						return ia.getAddress();
-					}
+			if(clientUDPThread.reciveAudioSocket.mediaPlayer!=null) {
+				if (clientUDPThread.reciveAudioSocket.mediaPlayer.isPlaying()) {
+					clientUDPThread.reciveAudioSocket.mediaPlayer.stop();
 				}
 			}
+			//status = true;
+			//startStreaming();
+			//ipAdress=editTextIpAdress.getText().toString();
+			//if(audioInfo!=null){
+				//path1=audioInfo.path;
+				//port=editTextPortAdress.getText().toString();
+			//	new SendAudioSocket(editTextIpAdress.getText().toString(),audioInfo);
+			//}else{
+				//song not playing
+			//}
 		}
-		return null;
-	}
-	public void setSourceAndStartPlay(File bufferFile, long size) {
-		try {
+	};
 
-//            mPlayerPosition = videoView.getCurrentPosition();
-//            videoView.setVideoPath(bufferFile.getAbsolutePath());
-
-			FileInputStream fis = new FileInputStream(bufferFile);
-			FileDescriptor fileDescriptor = fis.getFD();
-			mediaPlayer.setDataSource(fileDescriptor, 0, size);
-
-			mediaPlayer.prepare();
-			mediaPlayer.start();
-//            videoView.start();
-
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
+	void startReciveAudioSocket(ProgressBar progerssBar,Context context){
+		if (serverUDPThread != null) {
+			serverUDPThread.sockett.close();
+			Thread dummy = serverUDPThread;
+			serverUDPThread = null;
+			dummy.interrupt();
 		}
+		serverUDPThread = new ServerUDPThread(MediaController.getInstance().getPlayingSongDetail(),progerssBar,context);
+		serverUDPThread.start();
 	}
+	void startSendAudioSocket(ProgressBar progressBarClient){
+		if (clientUDPThread != null) {
+			Thread dummy = clientUDPThread;
+			clientUDPThread = null;
+			dummy.interrupt();
+		}
+		clientUDPThread = new ClientUDPThread(progressBarClient);
+		clientUDPThread.start();
+		//ipTextView.setText("Ip: "+ Utils.getIPAddress(true));
+		//portTextView.setText("Port: "+clientUDPThread.reciveAudioSocket.serverSocket.getLocalPort());
+		//portTextView.setText("Port: 5005");
+	}
+
 }
