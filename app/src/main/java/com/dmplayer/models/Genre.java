41c2daa0af3482ed22a8b9c1045fa04a1d550575
yourMomsBoyfriend <diagorus@ukr.net>
2016-10-06 13:49:25 +0300

    forgotten files for last commit

---
+++ app/src/main/java/com/dmplayer/models/Genre.java
@@ -0,0 +1,19 @@
+package com.dmplayer.models;
+
+public class Genre {
+    private final int id;
+    private final String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
